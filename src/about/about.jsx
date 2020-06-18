af7bdd519bc7fc40b39efa36101117374f1a859d
gbochikubo <gbochikubo@estudante.ufla.br>
2020-06-18 08:24:58 -0300

    adicionado frontend da aplicação

---
+++ src/about/about.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props =>{
+    return (
+        <div>
+            <PageHeader name='Tarefas' small='Cadastro'></PageHeader>
+            <h2>Nossa História</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+            <h2>Missão</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+            <h2>Visão</h2>
+            <p>Lorem ipsum dolor sit amet...</p>
+
+        </div>
+    )
+}
